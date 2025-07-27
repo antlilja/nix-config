221e8dfdb53badce2175bf88114ef25cc96febbb
antlilja <liljaanton2001@gmail.com>
2025-07-27 18:49:09 +0200

    Enable bluetui when bluetooth is enabled

--- modules/system/bluetooth.nix
+++ modules/system/bluetooth.nix
@@ -11,6 +11,9 @@ in
   config = mkIf cfg.enable {
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+    environment.systemPackages = with pkgs; [
+      bluetui
+    ];
     impermanence.rootDirs = [
       "/var/lib/bluetooth"
     ];
