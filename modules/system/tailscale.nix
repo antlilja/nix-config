aca4d4a43844b9183e4637c03bb8af1fe7379b82
antlilja <liljaanton2001@gmail.com>
2025-09-09 23:35:27 +0200

    Tailscale error workaround

--- modules/system/tailscale.nix
+++ modules/system/tailscale.nix
@@ -9,6 +9,21 @@ in
   };
 
   config = mkIf cfg.enable {
+    nixpkgs.overlays = [
+      (_: prev: {
+        tailscale = prev.tailscale.overrideAttrs (old: {
+          checkFlags =
+            builtins.map
+              (
+                flag:
+                if prev.lib.hasPrefix "-skip=" flag
+                then flag + "|^TestGetList$|^TestIgnoreLocallyBoundPorts$|^TestPoller$"
+                else flag
+              )
+              old.checkFlags;
+        });
+      })
+    ];
     services.tailscale.enable = true;
 
     impermanence = {
