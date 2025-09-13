8eebf08694274cb58d26cf9c36971c3ec9195647
antlilja <liljaanton2001@gmail.com>
2025-09-13 23:53:22 +0200

    Set console keymap to sv-latin1 and make most locales swedish

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -5,10 +5,24 @@ let cfg = config.system.locale;
 in
 {
   config = {
-    i18n.defaultLocale = "en_US.UTF-8";
+    i18n = {
+      defaultLocale = "en_US.UTF-8";
+      extraLocaleSettings = {
+        LC_ADDRESS = "sv_SE.UTF-8";
+        LC_MEASUREMENT = "sv_SE.UTF-8";
+        LC_MESSAGES = "en_US.UTF-8";
+        LC_MONETARY = "sv_SE.UTF-8";
+        LC_NAME = "sv_SE.UTF-8";
+        LC_NUMERIC = "sv_SE.UTF-8";
+        LC_PAPER = "sv_SE.UTF-8";
+        LC_TELEPHONE = "sv_SE.UTF-8";
+        LC_TIME = "sv_SE.UTF-8";
+        LC_COLLATE = "sv_SE.UTF-8";
+      };
+    };
     console = {
       font = "Lat2-Terminus16";
-      useXkbConfig = true;
+      keyMap = "sv-latin1";
     };
   };
 }
