4be59ceb8afd7468f73f4ee04b9aa25ce280e6b5
antlilja <liljaanton2001@gmail.com>
2025-09-14 03:55:19 +0200

    hyprland add hyprlock

--- modules/desktops/hyprland/default.nix
+++ modules/desktops/hyprland/default.nix
@@ -57,6 +57,7 @@ in
             "$mod, C, killactive"
             "$mod SHIFT, R, exec, reboot"
             "$mod SHIFT, S, exec, shutdown now"
+            "$mod SHIFT, L, exec, hyprlock"
             "$mod, Period, split:swapactiveworkspaces, current +1"
             "$mod, PRINT, exec, slurp | grim -g - ~/$(date \"+%T_%d-%m-%Y\").png"
           ]
@@ -102,6 +103,66 @@ in
             no_hardware_cursors = "1";
             enable_hyprcursor = false;
           };
+        };
+      };
+      programs.hyprlock = {
+        enable = true;
+        settings = {
+          general = {
+            disable_loading_bar = true;
+            grace = 0;
+            hide_cursor = true;
+            no_fade_in = true;
+          };
+
+          background = [
+            {
+              path = "screenshot";
+              blur_passes = 3;
+              blur_size = 8;
+            }
+          ];
+
+          input-field = [
+            {
+              size = "180, 35";
+              outline_thickness = 1;
+              dots_size = 0.2;
+              dots_spacing = 0.2;
+              dots_center = true;
+              outer_color = "rgba(0, 0, 0, 0)";
+              inner_color = "rgba(100, 114, 125, 0.4)";
+              font_color = "rgb(200, 200, 200)";
+              fade_on_empty = false;
+              font_family = "SF Pro Roundeds";
+              placeholder_text = "<span foreground=\"##ffffff99\">Enter Password</span>";
+              hide_input = false;
+              position = "0, -445";
+              halign = "center";
+              valign = "center";
+            }
+          ];
+          label = [
+            {
+              text = "cmd[update:1000] echo -e \"$(date +\"%A, %d %B\")\"";
+              color = "rgba(216, 222, 233, 0.70)";
+              font_size = 23;
+              font_family = "SF Pro Rounded Semi-Bold";
+              position = "0, 375";
+              halign = "center";
+              valign = "center";
+            }
+            {
+              text = "cmd[update:1000] echo \"<span>$(date +\"%H:%M\")</span>\"";
+              color = "rgba(216, 222, 233, 0.70)";
+              font_size = 110;
+              font_family = "SF Pro Rounded Semi-Bold";
+              outline_thickness = "5";
+              position = "0, 280";
+              halign = "center";
+              valign = "center";
+            }
+          ];
         };
       };
       services.dunst.enable = true;
